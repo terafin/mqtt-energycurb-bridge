aa2c70406a38675e6bc4255b47970e43524f2b3d
Justin Wood <woody@apple.com>
2017-04-18 23:59:12 -0700

    working version

--- mqtt-energycurb-bridge.js
+++ mqtt-energycurb-bridge.js
@@ -3,29 +3,35 @@ mqtt = require('mqtt')
 
 logging = require('./homeautomation-js-lib/logging.js')
 mqtt_helpers = require('./homeautomation-js-lib/mqtt_helpers.js')
+energycurb = require('./homeautomation-js-lib/energycurb.js')
 
 
 // Config
-host = process.env.MQTT_HOST
+dst_host = process.env.MQTT_HOST
+topic_prefix = process.env.ENERGY_CURB_PREFIX
+username = process.env.ENERGY_CURB_USER
+password = process.env.ENERGY_CURB_PASS
 
 // Set up modules
-logging.set_enabled(fatruelse)
+logging.set_enabled(true)
+
 
 // Setup MQTT
-client = mqtt.connect(host)
+dst_client = mqtt.connect(dst_host)
 
 // MQTT Observation
 
-client.on('connect', () => {
+dst_client.on('connect', () => {
     logging.log('Reconnecting...\n')
-    client.subscribe("#")
 })
 
-client.on('disconnect', () => {
+dst_client.on('disconnect', () => {
     logging.log('Reconnecting...\n')
-    client.connect(host)
+    dst_client.connect(dst_host)
 })
 
-client.on('message', (topic, message) => {
-    logging.log(" " + topic + ":" + message)
+energycurb.set_user_pass(username, password)
+energycurb.set_client_callback(function(topic, message) {
+    logging.log("topic: " + topic + "   message: " + message)
+    mqtt_helpers.publish(dst_client, topic, message)
 })
